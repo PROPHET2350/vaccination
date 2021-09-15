b70a8fed8048594d5b56793cb4b00af43e316ade
PROPHET2350 <jorgeluis33ariosa@gmail.com>
2021-09-15 16:32:54 -0500

    create default admin user

--- vaccination/src/main/java/com/challenge/vaccination/VaccinationApplication.java
+++ vaccination/src/main/java/com/challenge/vaccination/VaccinationApplication.java
@@ -1,13 +1,27 @@
 package com.challenge.vaccination;
 
+import com.challenge.vaccination.Models.Employees;
+import com.challenge.vaccination.Models.Users;
+import com.challenge.vaccination.Services.EmployeesServices;
+import com.challenge.vaccination.Services.UsersServices;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VaccinationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaccinationApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner run(UsersServices usersServices, EmployeesServices employeesServices){
+		return args -> {
+			Employees emp = employeesServices.CreateEmployee(new Employees("admin",
+					"1234567890","notname","not","admin@admin.com"));
+			usersServices.CreateUser(new Users("adminId","admin","root","ADMIN",emp));
+		};
 	}
 
 }
