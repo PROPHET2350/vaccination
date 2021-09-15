8353cf41d81aeca4d3f4281e6b4ee86791d7ffca
PROPHET2350 <55221023+PROPHET2350@users.noreply.github.com>
2021-09-15 18:19:27 -0500

    Update EmployeesServices.java

--- vaccination/src/main/java/com/challenge/vaccination/Services/EmployeesServices.java
+++ vaccination/src/main/java/com/challenge/vaccination/Services/EmployeesServices.java
@@ -1,7 +1,10 @@
 package com.challenge.vaccination.Services;
 
 import com.challenge.vaccination.Models.Employees;
+import com.challenge.vaccination.Repositories.EmployeesDetailsRepository;
 import com.challenge.vaccination.Repositories.EmployeesRepository;
+import com.challenge.vaccination.Repositories.UsersRepository;
+import com.challenge.vaccination.Repositories.VaccinationDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,20 @@ public class EmployeesServices {
 
     @Autowired
     private EmployeesRepository employeesRepository;
+    @Autowired
+    private VaccinationDetailsRepository vaccinationDetailsRepository;
+    @Autowired
+    private EmployeesDetailsRepository employeesDetailsRepository;
+    @Autowired
+    private UsersRepository usersRepository;
 
-    public EmployeesServices(EmployeesRepository employeesRepository) {
+    public EmployeesServices(EmployeesRepository employeesRepository, VaccinationDetailsRepository vaccinationDetailsRepository, EmployeesDetailsRepository employeesDetailsRepository, UsersRepository usersRepository) {
         this.employeesRepository = employeesRepository;
+        this.vaccinationDetailsRepository = vaccinationDetailsRepository;
+        this.employeesDetailsRepository = employeesDetailsRepository;
+        this.usersRepository = usersRepository;
     }
+
     public Employees CreateEmployee(Employees employee){
         return this.employeesRepository.save(employee);
     }
@@ -24,6 +37,18 @@ public class EmployeesServices {
         return this.employeesRepository.findByEmployeeid(id);
     }
     public void DeleteEmployee(String id){
+        String userid = usersRepository.findByEmployeeEmployeeid(id).getUserid();
+        String empdetails = employeesDetailsRepository.findByEmployeeEmployeeid(id).getEmployeesdetailsid();
+        String vaccinationid = vaccinationDetailsRepository.findByEmployeesDetailsEmployeesdetailsid(empdetails).getVaccinationdetailsid();
+        if (userid != null){
+            usersRepository.deleteById(userid);
+        }
+        if (vaccinationid != null){
+            vaccinationDetailsRepository.deleteById(vaccinationid);
+        }
+        if (empdetails != null){
+            employeesDetailsRepository.deleteById(empdetails);
+        }
       this.employeesRepository.deleteById(id);
     }
     public List<Employees> searchAll(){
